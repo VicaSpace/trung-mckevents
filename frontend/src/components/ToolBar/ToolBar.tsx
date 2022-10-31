0594e3750f4dbd41590e4a66d2169488573fc6ee
trungnq2k <Trung_Ngo@mckinsey.com>
2022-10-31 23:17:59 +0700

    frontend: add toolbar

--- frontend/src/components/ToolBar/ToolBar.tsx
+++ frontend/src/components/ToolBar/ToolBar.tsx
@@ -1,7 +1,11 @@
 import "./ToolBar.css";
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosArrowDown } from "react-icons/io";
 
 const ToolBar = () => {
+  const toggleFilterDropdown = () => {
+    document.getElementById("myDropdown")?.classList.toggle("show");
+  };
   return (
     <div className="toolbar-container">
       <div className="create-event">
@@ -15,11 +19,58 @@ const ToolBar = () => {
                 //   onChange={(event) => handleSearchNote(event.target.value)}
                 type="text"
                 placeholder="input search text"
+                className="search-input"
               />
-              <AiOutlineSearch className="search-icon" size="1.3em" />
+              <button className="search-button">
+                <AiOutlineSearch size="1.3em" />
+              </button>
             </div>
           </div>
-          <div className="filter-sort"></div>
+          <div className="filter-sort">
+            <button
+              className="filter-sort-btn"
+              onClick={(e) => toggleFilterDropdown()}
+            >
+              Filter <IoIosArrowDown className="arrow-down" />
+            </button>
+            <div id="myDropdown" className="dropdown-content">
+              <ul className="filter-area">
+                <li>
+                  <label htmlFor="date">
+                    <input
+                      className=""
+                      type="checkbox"
+                      name="filter-options"
+                      value="date"
+                    />
+                    By Date
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="office">
+                    <input
+                      className=""
+                      type="checkbox"
+                      name="filter-options"
+                      value="office"
+                    />
+                    By Office
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="my-event">
+                    <input
+                      className=""
+                      type="checkbox"
+                      name="filter-options"
+                      value="my-event"
+                    />
+                    My event
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="location">filter-sort</div>
       </div>
